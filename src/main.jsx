bca72b0f9019914fc9f3a1494237517401611f41
Helder Oliveira <h.manuel@aluno.ifsp.edu.br>
2026-03-10 17:02:14 -0300

    Versão 1.0

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+import './index.css'
+import App     from './pages/App.jsx'
+import Home    from './pages/Home.jsx'
+import Sobre   from './pages/Sobre.jsx'
+import Contato from './pages/Contato.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+       <Routes>
+        <Route index element={<Home />} />
+        <Route path='/app' element={<App />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='/contato' element={<Contato />} />
+        
+       </Routes>
+    </BrowserRouter>
+    <App />
+  </StrictMode>,
+)
